4eba9b0e6a849cb744d5b97d2b634e0b2b9a01c9
Velikss <felixbeuving@live.nl>
2018-05-07 14:39:11 +0200

    Created abstract algorithm class

--- src/tsp/kbs2/Algorithm.java
+++ src/tsp/kbs2/Algorithm.java
@@ -5,11 +5,61 @@
  */
 package tsp.kbs2;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author Felix
  */
 public abstract class Algorithm {
-    private int test;
-    
+
+    private Long time;
+    private String name;
+    private int simulatieNr;
+    private float afstand;
+    private ArrayList<Product> products = new ArrayList<>();
+
+    public void solve(ArrayList<Product> locaties) {
+    }
+
+    public Long getTime() {
+        return time;
+    }
+
+    public void setTime(Long time) {
+        this.time = time;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getSimulatieNr() {
+        return simulatieNr;
+    }
+
+    public void setSimulatieNr(int simulatieNr) {
+        this.simulatieNr = simulatieNr;
+    }
+
+    public float getAfstand() {
+        return afstand;
+    }
+
+    public void setAfstand(float afstand) {
+        this.afstand = afstand;
+    }
+
+    public ArrayList<Product> getProducts() {
+        return products;
+    }
+
+    public void setProducts(ArrayList<Product> products) {
+        this.products = products;
+    }
+
 }
