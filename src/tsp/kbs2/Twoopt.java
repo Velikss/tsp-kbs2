b73901b6d1262255e5cff0ab90134c2db970221d
Velikss <felixbeuving@live.nl>
2018-05-18 13:19:14 +0200

    Changes to Twoopt

--- src/tsp/kbs2/Twoopt.java
+++ src/tsp/kbs2/Twoopt.java
@@ -11,19 +11,23 @@ import java.util.ArrayList;
  * @author Felix
  */
 public class Twoopt extends Algorithm {
-    
+
     public Twoopt() {
         super.setName("Two-opt Algorithm");
     }
-    
-//    @Override
-//    public void solve(ArrayList<Location> locations) {
-//        long startTime = System.nanoTime() / 1000;
-//        ArrayList<Location> currentSolution = new ArrayList<>();
-//        
-//        for (Location loc : locations) {
-//            
-//        }
-//    }
-    
+
+    @Override
+    public Route solve(ArrayList<Location> locations) {
+        time = System.nanoTime();
+        ArrayList<Location> currentSolution = new ArrayList<>();
+        currentSolution.addAll(locations);
+        int size = currentSolution.size();
+        for (int i = 0; i < size; i++) {
+            for (Location loc : locations) {
+
+            }
+        }
+        return result;
+    }
+
 }
